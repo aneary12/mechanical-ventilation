518f8ccf04e70317b845f15d7b78583e9f6fb2f6
Andy Neary <a.neary6@nuigalway.ie>
2021-02-17 11:55:19 +0000

    Initial patient selection

---
+++ queries/icu_patients.sql
@@ -0,0 +1,18 @@
+SELECT
+    i.subject_id,
+    i.hadm_id,
+    i.icustay_id,
+    p.gender,
+    p.dob,
+    ROUND((CAST(a.admittime AS date) - CAST(p.dob AS date))/365.242,2) AS age,
+    a.admittime,
+    a.dischtime,
+    i.intime,
+    i.outtime,
+    i.los,
+    a.diagnosis,
+    a.hospital_expire_flag
+FROM
+    icustays i
+    INNER JOIN patients p ON i.subject_id = p.subject_id
+    INNER JOIN admissions a ON p.subject_id = a.subject_id
